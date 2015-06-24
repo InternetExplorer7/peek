096e245673cb38c51c81ccf156d963ed0c3435af
InternetExplorer7 <themaster231436@gmail.com>
2015-06-23 19:25:29 -0700

    Peek NodeJS

---
+++ index.js
@@ -0,0 +1,85 @@
+  var wit = require('node-wit');
+  var fs = require('fs');
+  var ACCESS_TOKEN = "DVK7ANMWGHHZ22GFGR6OBPCNDQ4KNS2E";
+  /* WIT.AI */
+
+/* SOCKET.IO */ 
+
+  var express = require('express');
+  var app = express();
+  var cons = require('consolidate');
+  var mongodb = require('mongodb');
+  var ObjectId = require('mongodb').ObjectId;
+    app.use(express.static(__dirname + '/public'));
+
+  /* MONGO DB */
+var MongoClient = require('mongodb').MongoClient; // MONGO DB
+var Server = require('mongodb').MongoClient;
+  /* MONGO DB */ 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+require('es6-shim');
+
+
+  MongoClient.connect('mongodb://Kavehk:Kevin231@ds051740.mongolab.com:51740/peek', function(err, db){
+
+    io.on('connection', function(socket){
+    console.log('a user connected');
+    /* NEW USER CONNECTED */
+    socket.on('newuser', function(id, callback){ // Get the JSON and send it back
+      console.log("ID in index.js " + id);
+      db.collection('con').findOne({_id: id}, function(err, doc){
+        callback(doc);
+      });
+    });
+
+    /* PRESSED PLAY */
+    socket.on('play', function(id){
+      console.log('got to play ' + id);
+      db.collection('con').update({_id: id}, { $set: { play: 1, started: 1 }}, {upsert: true});
+      change(id);
+    });
+
+    /* PRESSED PAUSE */
+    socket.on('pause', function(id){
+      db.collection('con').update({_id: id}, { $set: { play: 0  } }, {upsert: true});
+      change(id);
+    });
+
+    function change(id){
+      setTimeout(function(){
+        db.collection('con').findOne({_id: id}, function(err, doc){
+        io.emit('update', doc);
+        });
+      }, 1000);
+    }
+
+
+  socket.on('start', function(vid, ip, id){ // User has created video, time to insert to new table
+    var video;
+    console.log('started ' + vid + ' ' + ip);
+    video = vid.substring(vid.indexOf("=") + 1); // Video URL
+    insert(video, ip, id)// Insert new table into collection
+  });
+
+  function insert(video, ip, id){
+    db.collection('con').insert({_id: id, host: ip, url: video, play: 0, started: 0, chat: []}) // Setting ID, host, YouTube Video, Play Status (0 === pause) and Chat arr.
+  };
+
+
+
+
+});
+
+
+
+  }); // MONGO CLIENT
+
+
+  app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on *:3000');
+}); 
---
+++ public/play.js
@@ -0,0 +1,92 @@
+
+ var socket = io();
+
+    // 2. This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+    var player;
+    var id = 'M7lc1UVf-VE';
+
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+
+    // 4. The API will call this function when the video player is ready.
+    function onPlayerReady(event) {
+        //player.playVideo();
+    }
+
+    // 5. The API calls this function when the player's state changes.
+    //    The function indicates that when playing a video (state=1),
+    //    the player should play for six seconds and then stop.
+    var done = false;
+
+    function onPlayerStateChange(event) {
+        console.log("Player state changed");
+    }
+
+
+    function stopVideo() {
+        player.stopVideo();
+    }
+
+     var uid = window.location.href;
+    uid = uid.substring(uid.indexOf("#") + 1);
+    console.log("ID play.js " + uid); 
+    uid = parseInt(uid);
+    socket.emit('newuser', uid, function(data){
+      id = "http://www.youtube.com/embed/" + data.url + "?enablejsapi=1&controls=0";
+      document.getElementById("player").src = id;
+      console.log(JSON.stringify(data));
+      if(data.started === 1){
+        window.location.replace("missed.html");
+      }
+
+
+
+       $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
+            function(json) {
+                if(json.ip === data.host){ // User is host
+                  $("#start").removeAttr("disabled");
+                  $("#pause").removeAttr("disabled");
+                }
+            });
+
+                /*DUMP ARR INTO CHAT-BOX*/
+    });
+
+
+
+    /* PLAY IS PRESSED */
+
+    $("#start").click(function(){
+      console.log('sending ID to play: ' + uid);
+      socket.emit('play', uid);
+    });
+
+    $("#pause").click(function(){
+      socket.emit('pause', uid);
+    });
+
+    socket.on('update', function(data){ // Left off here
+      if(data._id === uid){ // User IDs match  
+        console.log("DATA PLAY: " + data.play)
+        if(data.play === 1){ // play
+          player.playVideo();
+        }
+        else{ // User paused
+          player.pauseVideo();
+        }
+      } 
+    });
