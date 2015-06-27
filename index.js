b8b3ba535b3b3c9a67aa75b9c4bbf176a218b1df
InternetExplorer7 <themaster231436@gmail.com>
2015-06-27 13:53:28 -0700

    Time change

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ require('es6-shim');
         db.collection('con').findOne({_id: id}, function(err, doc){
         io.emit('updatemsg', doc);
         });
-      }, 1000);
+      }, 2000);
     }
 
     function change(id){
@@ -68,7 +68,7 @@ require('es6-shim');
         db.collection('con').findOne({_id: id}, function(err, doc){
         io.emit('update', doc);
         });
-      }, 2000);
+      }, 9000);
     }
 
 
