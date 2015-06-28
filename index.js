067436076dbe5cc37ca9c1bb9e9f3df4d27ffb2b
InternetExplorer7 <themaster231436@gmail.com>
2015-06-27 19:16:21 -0700

    time

--- index.js
+++ index.js
@@ -69,14 +69,14 @@ require('es6-shim');
         db.collection('con').findOne({_id: id}, function(err, doc){
         io.emit('updatemsg', doc);
         });
-      }, 1000);
+      }, 300);
     }
     function change(id){
       setTimeout(function(){
         db.collection('con').findOne({_id: id}, function(err, doc){
         io.emit('update', doc);
         });
-      }, 1200);
+      }, 600);
     }
 
   socket.on('start', function(vid, ip, id){ // User has created video, time to insert to new table
